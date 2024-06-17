27bf0981d150580cf8fbf47cf90aa85cc086e365
Conor-Behard333 <conorbehard@gmail.com>
2024-06-17 19:41:53 +0100

    update

--- basic/simple-http-server.py
+++ basic/simple-http-server.py
@@ -2,12 +2,18 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 def run():
     port = 8080
+    
+    # Create the server class
+    server_class=HTTPServer
+
+    # Create the simple http request handler class (a handler is a block of code that gets triggered when a certain event or request happens)
+    handler_class=SimpleHTTPRequestHandler
 
     # Define server address and use the port variable as the port number 
     server_address = ('', port)  
 
     # Create a HTTP server
-    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd = server_class(server_address, handler_class)
 
     # Tell the user which port the server is running on
     print(f'Server running on port {port}. Go to http://localhost:{port}')
