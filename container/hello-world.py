3379177caaf4e19239ae8963c00c9bd4adaaf570
Conor Bèhard Roberts <47724661+Conor-Behard333@users.noreply.github.com>
2024-06-19 08:47:01 +0100

    Update hello-world.py

--- container/hello-world.py
+++ container/hello-world.py
@@ -1,29 +1,36 @@
 import http.server
 import socketserver
+import os
 
-class CustomHandler(http.server.BaseHTTPRequestHandler):
-
-    # When a get request is sent to http://localhost:8080 this function runs
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        html_content = ""
-        return_code = 200
-        try:
-            # Open the index.html file, read it and store its contents in the variable "html_content"
-            with open('index.html', 'rb') as file:
-                html_content = file.read()
-        except:
-            # If the code within the try block throws an error then run this block of code
-            print("something went wrong")
-            # 500 means an internal server error occurred
-            return_code = 500 
+        # Define paths and corresponding HTML files
+        routes = {
+            "/": "index.html",
+            # "/about": "about.html",
+            # "/fun-page": "fun.html",
+        }
 
-        # Send a code response to the client (200 means everything worked)
-        self.send_response(return_code)
-        # Tells the client what type of content it is about to receive
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        # Get the requested path
+        requested_path = self.path
 
-        self.wfile.write(html_content)
+        # Get the file to serve based on the path
+        file_to_serve = routes.get(requested_path, None)
+
+        if file_to_serve:
+            # Serve the corresponding HTML file if it exists
+            if os.path.exists(file_to_serve):
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                with open(file_to_serve, 'rb') as file:
+                    self.wfile.write(file.read())
+            else:
+                # If the file doesn't exist, serve a 404 error
+                self.send_error(404, "File not found")
+        else:
+            # If the path is not in the routes, serve a 404 error
+            self.send_error(404, "File not found")
 
 def run():
     port = 8080
@@ -35,7 +42,7 @@ def run():
     server_address = ('', port)
 
     # Create the http process
-    httpd = socketserver.TCPServer(("", port), handler)
+    httpd = socketserver.TCPServer(server_address, handler)
 
     # Tell the user which port the server is running on
     print(f'Server running on port {port}. Go to http://localhost:{port}')
